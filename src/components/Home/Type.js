65bd2a5cef68057c4e6b16cec365eea1544abef1
Shivansh Bahl <96385304+shivanshb07@users.noreply.github.com>
2024-07-09 10:33:35 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["Software Developer", "MERN Stack Developer"],
+        strings: ["Software Test Engineer","Software Developer", "MERN Stack Developer"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
