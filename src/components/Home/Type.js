3d28b9b67273a8ffb299e8da78c327199ec76130
Shivansh Bahl <96385304+shivanshb07@users.noreply.github.com>
2024-08-23 15:08:36 +0530

    Update Type.js
    TPA addition

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["Software Test Engineer","Software Developer", "MERN Stack Developer"],
+        strings: ["Technical Project Associate","Software Test Engineer","Software Developer", "MERN Stack Developer"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
